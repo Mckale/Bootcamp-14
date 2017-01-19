ce64face3b59418d4ff1752c5b8e206a30f74f01
Makale <mckale@github.com>
2017-01-19 23:43:11 +0300

    Andelabs Home- Day 1 (FizzBuzz)

---
+++ Day-1/FizzBazz.py
@@ -0,0 +1,11 @@
+
+def fizz_buzz(number):
+        print("Fizz"*(not number % 3) + "Buzz"*(not number % 5) or number)
+
+fizz_buzz(5)
+fizz_buzz(25)
+fizz_buzz(3)
+fizz_buzz(33)
+fizz_buzz(15)
+fizz_buzz(101)
+fizz_buzz(8)
